1f4519c2985e554b922625ebec95947f4f954852
xuandanh <xuandanh321@gmail.com>
2021-01-14 11:29:29 +0700

    create new

---
+++ WebOnline.Applica/Common/IStorageSevice.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebOnline.Applica.Common
+{
+    public interface IStorageSevice
+    {
+        string GetFileUrl(string Filename);
+        Task SaveFileAync(Stream midiaBinarystream, string filename);
+        Task DeleteAync(string fileName);
+    }
+}
